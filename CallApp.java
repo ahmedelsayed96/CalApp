f5d2cf427c6aa2c72e0e17f3c13ca16065739674
Ahmed Elsayed <ahmed.elsayed.ali20@gmail.com>
2022-09-09 23:34:24 +0200

    intiating call app

---
+++ CallApp.java
@@ -0,0 +1,11 @@
+class CallApp {
+    
+    public static void main(String[] args) {
+    String[] dropdownValues  =  new String[] {"All Activities","Ignore" ,"Custom"};
+    int x = 5;
+    int y = 3;
+    int res = x + y;
+    System.out.println("Hello From Call App :"+res);
+        
+    }
+}
